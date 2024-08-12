b5884aa8e3b0318d3dee71180a829edd61e949b8
Bartosz Weselak <toszek2000@gmail.com>
2024-08-12 19:02:15 +0200

    Fix calculation error

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from shiny import App, render, ui, reactive
+from shinyswatch import theme
 
 app_ui = ui.page_fluid(
     ui.page_navbar(
@@ -7,21 +8,25 @@ app_ui = ui.page_fluid(
         ui.nav_panel("C", "Page C content"),
         title="App with navbar",
         id="page",
+
     ),
 
-
+    ui.div(
     ui.h2("Interest Calculation Dashboard", style="text-align: center; margin-bottom: 30px;"),
 
     ui.panel_well(
         ui.row(
-            ui.column(4, ui.input_numeric("num1", "Deposit:", 0, width="100%")),
-            ui.column(4, ui.input_numeric("num2", "Interest Rate (%):", 0, width="100%")),
-            ui.column(4, ui.input_numeric("num3", "Period (Months):", 0, width="100%")),
+            ui.column(4, ui.input_numeric("deposit", "Deposit:", 0, width="100%")),
+            ui.column(4, ui.input_numeric("intrest", "Interest Rate (%):", 0, width="100%")),
+            ui.column(4, ui.input_numeric("period", "Period (Months):", 0, width="100%")),
         ),
         ui.input_action_button("calc_button", "Calculate", style="margin-top: 20px; width: 100%;"),
         ui.row(
           ui.h3("Calculation Results", style="margin-top: 20px; text-align: center;"),
-          ui.output_text_verbatim("result", placeholder=True),
+          ui.output_text_verbatim("profit", placeholder=True),
+          ui.output_text_verbatim("tax", placeholder=True),
+          ui.output_text_verbatim("sum_after_tax", placeholder=True),
+
         )
     ),
     ui.panel_well(
@@ -31,7 +36,10 @@ app_ui = ui.page_fluid(
            ui.p("test"),
 
        ),
-        ),
+    ),
+    style="background-color: #d4fdff;"
+
+    ),
     )
 
 
@@ -39,21 +47,35 @@ def server(input, output, session):
     @output
     @render.text
     @reactive.event(input.calc_button)
-    def result():
-        deposit = input.num1()
-        interest_rate = input.num2() / 100
-        period_months = input.num3()
-        capitalization = 12
-        period_years = period_months / 12
-        profit = deposit * (1 + (interest_rate / capitalization)) ** (capitalization * period_years)
-        tax = calc_tax(deposit, profit)
-        sum_after_tax = profit - tax
-        return f"Profit: {profit:.2f}\nTax: {tax:.2f}\nSum After Tax (19%): {sum_after_tax:.2f}"
+    def sum_after_tax():
+        profit=calc_profit( input.deposit(),(input.intrest() / 100),input.period())
+        tax=calc_tax(profit, input.deposit())
+        return profit-tax
 
-    def calc_tax(deposit, profit):
-        tax = (profit - deposit) * 0.19
-        return tax
+    @output
+    @render.text
+    @reactive.event(input.calc_button)
+    def tax():
+        profit=calc_profit(input.deposit(), (input.intrest() / 100), input.period())
+        return calc_tax(profit, input.deposit())
 
+    @output
+    @render.text
+    @reactive.event(input.calc_button)
+    def profit():
+        return calc_profit( input.deposit(),(input.intrest() / 100),input.period())
+
+
+
+
+def calc_profit(deposit, interest_rate,period_months):
+    capitalization = 12
+    period_years = period_months / 12
+    profit = deposit * (1 + (interest_rate / capitalization)) ** (capitalization * period_years)
+    return profit
+def calc_tax(profit,deposit):
+    tax = (profit - deposit) * 0.19
+    return tax
 
 app = App(app_ui, server)
 
